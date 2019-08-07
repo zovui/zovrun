dc520bc9a0515035d42ff5b6ce1c9c1abc15267f
张军 <819396183@qq.com>
2019-08-07 17:28:41 +0800

    优化

--- src/components/code-run/utils.js
+++ src/components/code-run/utils.js
@@ -3,8 +3,7 @@ export function getSource (source, type) {
     const closeTagRE = new RegExp(`<\/[^>]*${type}>`)
     const queryRE = new RegExp(`<${type}[^>]*>[\\s\\S]*?<\\/[^>]*${type}>`, 'gi')
     let arr = source.match(queryRE) || []
-    let result = arr.map((item) => {
+    return arr.map((item) => {
         return item.replace(openTagRE, '').replace(closeTagRE, '')
     })
-    return result
 }
